a0f763d2dccdb7a8e25a7a3594c98e2d38408a11
Ivan Suarez <ivan.suarez.fabara@gmail.com>
2022-05-11 17:43:51 -0500

    Added endpoint for creating users.

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,14 @@ FROM python:3.10-alpine
 ENV PYTHONUNBUFFERED 1
 
 WORKDIR /app
+COPY . /app/
 
 COPY requirements.txt /app/
 RUN apk add --update --no-cache postgresql-client
 RUN apk add --update --no-cache --virtual .tmp-build-deps \
-      gcc libc-dev linux-headers postgresql-dev
+    gcc libc-dev linux-headers postgresql-dev
 RUN pip install -r requirements.txt
 RUN apk del .tmp-build-deps
-
-COPY . /app/
 
 RUN adduser -D user
 USER user
--- app/settings.py
+++ app/settings.py
@@ -10,17 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t8suy9#rl%av8p2+71sqc4tcn9_as1r!w6qfalosi)oaen6tr2'
+SECRET_KEY = 'django-insecure-t8suy9#rl' \
+             '%av8p2+71sqc4tcn9_as1r!w6qfalosi)oaen6tr2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
